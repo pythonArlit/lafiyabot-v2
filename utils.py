61c6792b750eb7b3004bf01d01f4d0b2851c9195
pythonArlit <101594896+pythonArlit@users.noreply.github.com>
2025-12-18 21:41:22 +0000

    UTILIS

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
+# utils.py
 import time
-def is_spam(sender: str, last_used: dict) -> bool:
-    if sender in last_used and time.time() - last_used[sender] < 25:
-        return True
-    return False
 
-def update_last_used(sender: str, last_used: dict):
+SPAM_DELAY = 25  # secondes
+
+def is_spam(sender: str, last_used: dict) -> bool:
+    now = time.time()
+    last = last_used.get(sender, 0)
+    return (now - last) < SPAM_DELAY
+
+def update_last_used(sender: str, last_used: dict) -> None:
     last_used[sender] = time.time()
